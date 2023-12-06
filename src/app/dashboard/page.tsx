6c19a8e64659c30857b237590d4ec8ce20997240
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-12-06 17:32:54 +0100

    feat: menu dashboar created

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import ReloadIcon from "../assets/icons/Reload icon.svg";
+import DownArrowIcon from "../assets/icons/Arrow down.svg";
+import DollarIcon from "../assets/icons/Dollar.svg";
+import ListIcon from "../assets/icons/list.svg";
+
 const Page = () => {
-  return <div>Dashboard</div>;
+  return (
+    <main className="flex flex-row h-20 w-full  justify-between bg-orange-50 items-center shadow-md">
+      <div className="flex flex-row items-center">
+        <h1 className="text-4xl ml-4">Dashboard</h1>
+        <div className="ml-8 flex flex-row items-center ">
+          <Image src={ReloadIcon} alt="reload icon" className="w-5 cursor-pointer" />
+          <p className="ml-3 text-[#767676]">Last import was made: </p>
+        </div>
+      </div>
+      <div className="flex flex-row mr-5 ">
+        <div className="flex flex-row mr-5 cursor-pointer">
+          <Image src={ListIcon} alt="list icon" className="w-6 mr-[4px]" /> <Image src={DownArrowIcon} alt="down arrow icon" className="w-6" />
+        </div>
+        <div className="flex flex-row cursor-pointer">
+          <Image src={DollarIcon} alt="dollar icon" className="w-7" /> <Image src={DownArrowIcon} alt="down arrow icon" className="w-6" />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Page;
