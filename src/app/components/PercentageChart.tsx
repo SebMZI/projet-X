be5c5949a3c12bee789738fe49b5d7269ef7b3ae
SebMZI <49833807+SebMZI@users.noreply.github.com>
2023-12-07 16:57:16 +0100

    Update PercentageChart.tsx

--- src/app/components/PercentageChart.tsx
+++ src/app/components/PercentageChart.tsx
@@ -4,66 +4,11 @@ import { ApexOptions } from "apexcharts";
 import ReactApexChart from "react-apexcharts";
 
 const PercentageChart = () => {
-  let option = {
-    chart: {
-      height: 260,
-      //   type: "radialBar",
-    },
-  };
-  let serie: ApexOptions = {
-    series: [69],
-    colors: ["#5903A4"],
-    plotOptions: {
-      radialBar: {
-        hollow: {
-          margin: 0,
-          size: "60%",
-          background: "#ffffff",
-        },
-        track: {
-          dropShadow: {
-            enabled: true,
-            top: 2,
-            left: 0,
-            blur: 4,
-            opacity: 0.15,
-          },
-        },
-        dataLabels: {
-          name: {
-            offsetY: -20,
-            color: "#5903A4",
-            fontSize: "18px",
-          },
-          value: {
-            color: "#5903A4",
-            fontSize: "35px",
-            show: true,
-          },
-        },
-      },
-    },
-    fill: {
-      type: "gradient",
-      gradient: {
-        shade: "dark",
-        type: "vertical",
-        gradientToColors: ["#B45ECD"],
-        stops: [0, 100],
-      },
-    },
-    stroke: {
-      lineCap: "round",
-    },
-    labels: ["Day win"],
-  };
-
   let chartOptions: ApexOptions = {
     series: [69],
-
     chart: {
       height: 260,
-      //   type: "radialBar",
+      type: "radialBar",
     },
 
     colors: ["#5903A4"],
@@ -114,7 +59,6 @@ const PercentageChart = () => {
 
   return (
     <div className="w-full">
-      {/* Utilisez Chart à la place de react-apexcharts */}
       {<ReactApexChart options={chartOptions} series={chartOptions.series} type="radialBar" width="350" />}
     </div>
   );
