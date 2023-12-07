c79370d652adcfad433d9a65ef001b1e9557e052
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-12-07 16:44:37 +0100

    feat : commit

--- src/app/components/PercentageChart.tsx
+++ src/app/components/PercentageChart.tsx
@@ -1,15 +1,70 @@
 "use client";
 import { ApexOptions } from "apexcharts";
 import dynamic from "next/dynamic";
+import { useState, useEffect, useRef } from "react";
 
 // Utilisez dynamic pour importer ApexChart de manière dynamique côté client
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const PercentageChart = () => {
+  let option = {
+    chart: {
+      height: 260,
+      //   type: "radialBar",
+    },
+  };
+  let serie: ApexOptions = {
+    series: [69],
+    colors: ["#5903A4"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "60%",
+          background: "#ffffff",
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15,
+          },
+        },
+        dataLabels: {
+          name: {
+            offsetY: -20,
+            color: "#5903A4",
+            fontSize: "18px",
+          },
+          value: {
+            color: "#5903A4",
+            fontSize: "35px",
+            show: true,
+          },
+        },
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#B45ECD"],
+        stops: [0, 100],
+      },
+    },
+    stroke: {
+      lineCap: "round",
+    },
+    labels: ["Day win"],
+  };
+
   let chartOptions: ApexOptions = {
     chart: {
       height: 260,
-      type: "radialBar",
+      //   type: "radialBar",
     },
     series: [69],
     colors: ["#5903A4"],
@@ -58,12 +113,7 @@ const PercentageChart = () => {
     labels: ["Day win"],
   };
 
-  return (
-    <div className="w-full">
-      {/* Utilisez Chart à la place de react-apexcharts */}
-      {<Chart options={chartOptions} series={chartOptions.series} type="radialBar" width="350" />}
-    </div>
-  );
+  return <div className="w-full">{<Chart options={chartOptions} series={chartOptions.serie} type="radialBar" width="350" />}</div>;
 };
 
 export default PercentageChart;
--- src/app/utils/csvUtils.tsx
+++ src/app/utils/csvUtils.tsx
@@ -1,4 +1,6 @@
+import csvData from "../assets/data/ReportHistory-3095006.csv";
 const csvUtils = () => {
+  console.log(csvData, "yeh");
   return <div>Enter</div>;
 };
 
