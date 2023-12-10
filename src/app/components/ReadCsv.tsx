6370dffa6ad88c14a5e98e2e3bad680d008aa85a
SebMZI <mrgy.sebastien@gmail.com>
2023-12-10 18:33:51 +0100

    feat: parse csv to objects

--- src/app/components/ReadCsv.tsx
+++ src/app/components/ReadCsv.tsx
@@ -1,7 +1,77 @@
 "use client";
-import { useEffect} from "react";
+import { useEffect } from "react";
 import Papa from "papaparse";
 
+interface History {
+  date: string;
+  name: string;
+  company: string;
+  account: string;
+}
+
+interface Positions {
+  time: string;
+  position: string;
+  symbol: string;
+  type: string;
+  volume: number;
+  price: number;
+  stopLoss: number;
+  target: number;
+  commission: number;
+  swap: string;
+  profit: number;
+}
+
+interface Orders {
+  openTime: string;
+  order: string;
+  symbol: string;
+  type: string;
+  volume: number;
+  price: number;
+  stopLoss: number;
+  target: number;
+}
+
+interface Deals {
+  time: string;
+  deal: string;
+  symbol: string;
+  type: string;
+  direction: string;
+  volume: number;
+  price: number;
+  order: string;
+  commissions: number;
+  fee: number;
+  swap: number;
+  profit: number;
+  balance: number;
+}
+interface Results {
+  totalNetProfit: number;
+  profitFactor: number;
+  expectedPayoff: number;
+  recoveryFactor: number;
+  totalTrades: number;
+  profitTrades: string;
+  lossTrades: string;
+  shortTrades: number;
+  longTrades: number;
+  largestProfitTrade: number;
+  largestLossTrade: number;
+  averageProfitTrade: number;
+  averageLossTrade: number;
+}
+
+interface Data {
+  history: History;
+  positions: Positions[];
+  orders: Orders[];
+  deals: Deals[];
+  results: Results;
+}
 
 const MyComponent: React.FC = () => {
   useEffect(() => {
@@ -22,6 +92,7 @@ const MyComponent: React.FC = () => {
         complete(results) {
           const rows = results.data;
           const categoryIndices: number[] = [];
+          let obj: Data = {} as Data;
           rows.map((row: any, index: number) => {
             if (
               row[0].length > 0 &&
@@ -30,11 +101,77 @@ const MyComponent: React.FC = () => {
               categoryIndices.push(index);
             }
           });
-          const history = rows.slice(0, 5);
+
+          const history: any[] = rows.slice(0, 5);
+          obj.history = {
+            date: history[4][3],
+            name: history[1][3],
+            company: history[3][3],
+            account: history[2][3],
+          };
           const positions = rows.slice(categoryIndices[1], categoryIndices[2]);
+          obj.positions = positions
+            .slice(2, positions.length)
+            .map((position: any) => ({
+              time: position[0],
+              position: position[1],
+              symbol: position[2],
+              type: position[3],
+              volume: position[4],
+              price: position[5],
+              stopLoss: position[6],
+              target: position[7],
+              commission: position[10],
+              swap: position[11],
+              profit: position[12],
+            }));
+
           const orders = rows.slice(categoryIndices[2], categoryIndices[3]);
+          obj.orders = orders.slice(2, orders.length).map((order: any) => ({
+            openTime: order[0],
+            order: order[1],
+            symbol: order[2],
+            type: order[3],
+            volume: order[4],
+            price: order[5],
+            stopLoss: order[6],
+            target: order[7],
+          }));
           const deals = rows.slice(categoryIndices[3], categoryIndices[4]);
-          const result = rows.slice(rows.length - 13, rows.length);
+          obj.deals = deals.slice(2, deals.length - 20).map((deal: any) => ({
+            time: deal[0],
+            deal: deal[1],
+            symbol: deal[2],
+            type: deal[3],
+            direction: deal[4],
+            volume: deal[5],
+            price: deal[6],
+            order: deal[7],
+            commissions: deal[8],
+            fee: deal[9],
+            swap: deal[10],
+            profit: deal[11],
+            balance: deal[12],
+          }));
+          const result: any[] = rows.slice(rows.length - 13, rows.length);
+          obj.results = {
+            totalNetProfit: result[1][3],
+            profitFactor: result[2][3],
+            expectedPayoff: result[2][7],
+            recoveryFactor: result[3][3],
+            totalTrades: result[6][3],
+            profitTrades: result[7][7],
+            lossTrades: result[7][11],
+            shortTrades: result[6][7],
+            longTrades: result[6][11],
+            largestProfitTrade: result[8][7],
+            largestLossTrade: result[8][11],
+            averageProfitTrade: result[9][7],
+            averageLossTrade: result[9][11],
+          };
+          console.log(result);
+
+          console.log(obj);
         },
       });
     };
@@ -47,7 +184,7 @@ const MyComponent: React.FC = () => {
         fileInput.removeEventListener("change", handleFileChange);
       }
     };
-  }, []); 
+  }, []);
 
   return (
     <div>
